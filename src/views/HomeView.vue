299a50be4e1682bcac31b3e96779d8932c875827
paolafioraso <13paola.fioraso@gmail.com>
2026-08-26 10:06:52 +0200

    creazione bacheca immagini

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,14 @@ import ItemsList from '../components/ItemsList.vue'
 </script>
 
 <template>
-  <div>
-    <!-- Spazio superiore ridotto per spostare la scritta più in alto -->
-    <div style="margin-top: 25vh; margin-bottom: 4rem;">
+  <div style="padding: 0 3rem 0 1.5rem; box-sizing: border-box; width: 100%; max-width: 100vw; overflow-x: hidden;">
+    <div style="margin-top: 18vh; margin-bottom: 3rem;">
       <p style="font-family: 'Aeonik', sans-serif; font-size: 1.8rem; line-height: 1.4; color: #343434; margin: 0; padding: 0;">
         Visual catalog of <span style="color: #afafaf;">underground landscapes</span> documented during<br>
         the 2023 and 2025 expeditions to Brazil.
       </p>
     </div>
-    
-    <!-- Griglia degli item -->
+
     <ItemsList :showDetails="false" />
   </div>
 </template>
